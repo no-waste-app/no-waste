36f1ffa90a4ef564d4a01d06927febef4db4020d
Anna Zalewska <gremiumsoft@gmail.com>
2020-12-29 16:39:28 +0100

    set up react app.

---
+++ no-waste-gui/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  //frist simptle template only html5 with css
+    return (
+      <form id="contex-search" class="search-box" action="App.js" method="post">
+            <h1> Nie  marnuj</h1>
+          <input type="text" name="" placeholder="Podaj co najmniej 3 składniki"/>
+          <input type="submit" name="" value="Szukaj"/>
+      </form>
+  );
+}
+
+export default App;
