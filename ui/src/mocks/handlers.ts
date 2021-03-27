00dba42fef4e2992e1578782049aeef8f300d018
Jakub Nyckowski <nyckowski.jakub@gmail.com>
2021-03-27 02:44:49 -0400

    Build fix

--- ui/src/mocks/handlers.ts
+++ ui/src/mocks/handlers.ts
@@ -4,13 +4,21 @@ import { Ingredients, Product, Recipe } from "../api/nowaste";
 const recipes: Recipe[] = [
   {
     title: "Spaghetti",
-    ingredients: [{ name: "tomatoes" }, { name: "pasta" }],
+    ingredients: [
+      { name: "tomatoes", quantity: "2" },
+      { name: "pasta", quantity: "0.5lb" },
+    ],
+    directions: "Some directions",
     description: "",
     imgUrl: "",
   },
   {
     title: "Grilled chicken",
-    ingredients: [{ name: "chicken" }, { name: "olive" }],
+    ingredients: [
+      { name: "chicken", quantity: "1 whole chicken" },
+      { name: "olive", quantity: "2 tbps" },
+    ],
+    directions: "some different directions",
     description: "",
     imgUrl: "",
   },
